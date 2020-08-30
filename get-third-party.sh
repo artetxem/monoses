d7a33813ce6e6578f14ed753fdcb686a9717a083
Mikel Artetxe <artetxem@gmail.com>
2020-08-30 02:26:52 +0100

    Add new dependencies

--- get-third-party.sh
+++ get-third-party.sh
@@ -9,7 +9,13 @@ git clone 'https://github.com/moses-smt/mosesdecoder.git' moses
 cd moses
 git checkout RELEASE-4.0
 cd ..
+git clone 'https://github.com/pytorch/fairseq.git'
+cd fairseq
+git checkout v0.6.0
+cd ..
 git clone 'https://github.com/clab/fast_align.git'
 git clone 'https://github.com/artetxem/phrase2vec.git'
 git clone 'https://github.com/artetxem/vecmap.git'
+git clone 'https://github.com/rsennrich/subword-nmt.git'
+git clone 'https://github.com/mjpost/sacrebleu.git'
 cd "$CURRENT"
